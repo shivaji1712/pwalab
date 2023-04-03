b16f3b94c21ec6f9ee2e59b84c5e02056ffa082f
shivaji1712 <115250545+shivaji1712@users.noreply.github.com>
2023-04-03 15:51:44 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// install event
-self.addEventListener('install', evt => {
-  console.log('service worker installed');
-});
-
-// activate event
-self.addEventListener('activate', evt => {
-  console.log('service worker activated');
-});
